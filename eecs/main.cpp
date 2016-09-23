c824480e4d2e486487a945f82b7fe677055b25f2
Andrew Bosset <Drew@dhcp-10-105-90-12.wireless.northwestern.private>
2016-09-23 02:31:27 -0500

    Initial Commit

---
+++ eecs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eecs
+//
+//  Created by Andrew Bosset on 9/23/16.
+//  Copyright © 2016 Andrew Bosset. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
