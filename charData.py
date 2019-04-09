e61751335435a0aa83002016c135365b0c9aedef
wrhyme29 <joshuafields2019@u.northwestern.edu>
2019-04-08 21:19:40 -0500

    Updated to be charData to be Dict of Dicts

--- charData.py
+++ charData.py
@@ -3,20 +3,25 @@ def newCharList():
 	return charList
 
 def newChar(charList, name):
-	charList[name] = []
+	charList[name] = {}
 	return charList
 
 def addAttribute(charList, name, attr):
-	attr_list = charList[name]
-	attr_list.append(attr)
-	charList[name] = attr_list
+	attr_dict = charList[name]
+	attr_dict.[attr] = []
+	charList[name] = attr_dict
 	return charList
 
 def delAttribute(charList, name, attr):
-	attr_list = charList[name]
-	del_list = []
-	for a in attr_list:
-		if(a != attr):
-			del_list.append(a)
-	charList[name] = del_list
+	attr_dict = charList[name]
+	attr_dict.pop(attr)
+	charList[name] = attr_dict
+	return charList
+	
+def addValuesForAttribute(charList, name, attr, val):
+	attr_dict = charList[name]
+	attr_vals = attr_dict[attr]
+	attr_vals.append(val)
+	attr_dict[attr] = attr_vals
+	charList[name] = attr_dict
 	return charList
